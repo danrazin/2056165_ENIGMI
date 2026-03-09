37de6d9b313b597c5514d77378fd975acacc7d80
Andrea Piermartini <48278297+andpierm@users.noreply.github.com>
2026-03-09 10:18:22 +0100

    Implementing "consume_topic" function

--- collectors/collector_telemetry_ws.py
+++ collectors/collector_telemetry_ws.py
@@ -7,7 +7,7 @@ from collectors.normalizer import normalize_event
 
 BASE_WS = "ws://localhost:8080/api/telemetry/ws"
 
-# topic e schema associato
+# topic and associated schema
 TOPICS = [
     ("topic.power.v1", "mars/telemetry/solar_array"),
     ("topic.power.v1", "mars/telemetry/power_bus"),
@@ -24,7 +24,19 @@ TOPICS = [
 async def consume_topic(schema, topic):
 
     url = f"{BASE_WS}?topic={topic}"
-    # ...
+    while True:
+        try:
+            print(f"Connected to {topic}")
+            async with websockets.connect(url) as ws:
+                async for message in ws:
+                    payload = json.loads(message)
+                    events = normalize_event(schema, payload)
+                    for event in events:
+                        print(json.dumps(event, indent=2))
+
+        except Exception as e:
+            print(f"Error on {topic}: {e}")
+            await asyncio.sleep(3)
 
 async def main():
 
